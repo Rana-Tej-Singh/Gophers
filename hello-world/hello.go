e5110d04e74533a9e8e4af946b274bfa9e5896ec
Rana-Tej-Singh <17468009+Rana-Tej-Singh@users.noreply.github.com>
2020-05-31 19:33:52 +0530

    Hello World

---
+++ hello-world/hello.go
@@ -0,0 +1,8 @@
+package main      # Calling from main package
+
+import "fmt"    // Imported fmt(format) function.
+
+// function body covered by { }
+func main(){
+   fmt.Println("Hello World!!")
+}
